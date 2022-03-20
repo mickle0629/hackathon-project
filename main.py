ab7faf54ecf20b51aaad5e3dbd54928c28328ab4
mlaramie <mlaramie24@my.whitworth.edu>
2022-03-19 17:04:59 -0700

    SQL DB connections established

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+from venv import create
 import pygame, sys
+import mysql.connector
 pygame.init()
 # --------------------------------------------
 # Works Cited/Referenced
 # Code for displaying an image via Pygame: 
 # https://www.geeksforgeeks.org/python-display-images-with-pygame/#:~:text=There%20are%20four%20basic%20steps,drawn%20on%20it%2C%20using%20image. 
+# Code for DB Connection Stuff:
+# https://www.w3schools.com/python/python_mysql_insert.asp 
 #---------------------------------------------
+
+mydb = mysql.connector.connect(
+  host="192.9.227.213",
+  user="hackathon",
+  password="hackathon123",
+  database = "hackathon1"
+)
+#Create event will be used following a series of user inputs to add the corresponding information to the database.
+def createEvent(date, time, name, descr, location, Ev_type): # People count and ID will be auto updated.
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO Event_Info (Date, Time, Event_Name, Event_Descr, Event_Location, Event_Type) VALUES (%s, %s, %s, %s, %s, %s)"
+    val = (date, time, name, descr, location, Ev_type)
+    mycursor.execute(sql, val)
+    mydb.commit()
+# Testing create event here:
+# createEvent("2022-01-01", "3:00:00", "Smash Tournament", "Smashy Tourny", "Duvall", "Green")
+
 
 # Var with white RGB
 white = (255, 255, 255)
@@ -21,21 +42,19 @@ pygame.display.set_caption('yellowImg')
 # Make surface for the sprites to display onto
 map_background = pygame.image.load(r'./assets/campus-map.jpg')
 official_icon = pygame.image.load(r'./assets/Red.png')
-display_surface.blit(map_background, (-384, -170))
 unofficial_icon = pygame.image.load(r'./assets/Green.png')
 neutral_icon = pygame.image.load(r'./assets/Yellow.png')
+# Print the map
+display_surface.blit(map_background, (-384, -170))
+
 
 while True :  
-    # Repeated image display for the loop at the requested coordinates
-    # gets the pygame event stuff
-    # if event is QUIT, halt the program
+    # Loop keeps running until closing
     for event in pygame.event.get() :
         if event.type == pygame.QUIT :
   
-            # deactivates the pygame library
+            # deactivates the pygame library and quit program
             pygame.quit()
-  
-            # quit the program.
             quit()
         elif event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
